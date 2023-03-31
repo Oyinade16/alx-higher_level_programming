10f5426225744c816b4ede713e0cb67c2d65facd
Oyinade16 <109605435+Oyinade16@users.noreply.github.com>
2023-03-31 09:55:32 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Module to find the hightest number in an array
-"""
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
 
-
-def find_peak(list_of_integers):
-    """
-    Method to find hightest number in array
-    """
-    if bool(list_of_integers) is False:
-        return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
